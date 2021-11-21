1ae25fe0efb3357f10398afc51630cf1b1da0c76
Vu Thanh Trung <Trungvu027@gmail.com>
2021-11-22 00:06:28 +0700

    final

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,11 @@
-$(document).on("vclick", "#btn_beep", cordovaBeep);
+$(document).on("vclick", "#btn_beep", beepNoti);
 
-function cordovaBeep() {
+function beepNoti() {
     navigator.notification.beep(3);
 }
 
-$(document).on("vclick", "#btn_vibration", cordovaVibration);
+$(document).on("vclick", "#btn_vibration", vibrationNoti);
 
-function cordovaVibration() {
+function vibrationNoti() {
     navigator.vibrate(1000, 2000, 2000, 3000, 3000, 3000, 1000, 1000, 1000);
 }
